e271d1f346063fa0622b1466607d30578d1f7c24
Pavle Mijatovic <paul.mijatovic@gmail.com>
2016-03-06 22:52:33 +0100

    Modifications

--- Sherp/Libs/Configuration.swift
+++ Sherp/Libs/Configuration.swift
@@ -13,8 +13,7 @@ import Foundation
 let DEFAULTS = NSUserDefaults.standardUserDefaults()
 let userDefaultsKey_defaultLanguage = "USER_DEFAULT_LANGUAGE"
 
-
-
+// MARK: - URLs
 let usersReqUrlString = "http://jsonplaceholder.typicode.com/users"
 let albumsReqUrlString = "http://jsonplaceholder.typicode.com/albums"
 let photosForAlbumReqUrlString = "http://jsonplaceholder.typicode.com/photos"
